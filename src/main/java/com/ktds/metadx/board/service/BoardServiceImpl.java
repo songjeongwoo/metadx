a01b9733539ae52cd8ad650ca90f13518929837d
jeongwoo <nice920525@naver.com>
2022-08-12 16:10:13 +0900

    게시물 목록조회(REST 방식 및 Member 연결)

--- src/main/java/com/ktds/metadx/board/service/BoardServiceImpl.java
+++ src/main/java/com/ktds/metadx/board/service/BoardServiceImpl.java
@@ -8,9 +8,11 @@ import com.ktds.metadx.board.dto.BoardDTO;
 import com.ktds.metadx.board.mapper.BoardMapper;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class BoardServiceImpl implements BoardService {
     
     private final BoardMapper mapper;
