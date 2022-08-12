d098fbe4244ebd6c212016492608d9bdae2e3c84
jeongwoo <nice920525@naver.com>
2022-08-12 11:19:05 +0900

    게시물 상세조회(REST 방식 개발로 리팩토링)

--- src/main/java/com/ktds/metadx/board/controller/BoardController.java
+++ src/main/java/com/ktds/metadx/board/controller/BoardController.java
@@ -1,12 +1,12 @@
 package com.ktds.metadx.board.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ktds.metadx.board.dto.BoardDTO;
 import com.ktds.metadx.board.service.BoardService;
@@ -14,7 +14,8 @@ import com.ktds.metadx.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
-@Controller
+// @Controller
+@RestController
 @Log4j2
 @RequestMapping("/board")
 @RequiredArgsConstructor
@@ -22,13 +23,10 @@ public class BoardController {
 
     private final BoardService service;
 
-    // @GetMapping("/list")
-    // public ModelAndView getList() {
-    //     ModelAndView mv = new ModelAndView();
-    //     mv.addObject("boardList", service.getList());
-    //     mv.setViewName("board/list.html");
-    //     return mv;
-    // }
+    @GetMapping("/list")
+    public List<BoardDTO> getList() {
+        return service.getList();
+    }
 
     @GetMapping("/addPost")
     public void getAddPostPage() {
